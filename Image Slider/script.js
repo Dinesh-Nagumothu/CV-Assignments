00ebae5cbc2bc948b5c32ee6dede41417b12a28b
Dinesh <136153839+Dinesh-Nagumothu@users.noreply.github.com>
2025-03-12 16:14:05 +0530

    Add files via upload

---
+++ Image Slider/script.js
@@ -0,0 +1,24 @@
+const images = [
+    'https://fastly.picsum.photos/id/29/4000/2670.jpg?hmac=rCbRAl24FzrSzwlR5tL-Aqzyu5tX_PA95VJtnUXegGU', 
+    'https://fastly.picsum.photos/id/17/2500/1667.jpg?hmac=HD-JrnNUZjFiP2UZQvWcKrgLoC_pc_ouUSWv8kHsJJY',       
+    'https://fastly.picsum.photos/id/89/4608/2592.jpg?hmac=G9E4z5RMJgMUjgTzeR4CFlORjvogsGtqFQozIRqugBk',
+    'https://fastly.picsum.photos/id/119/3264/2176.jpg?hmac=PYRYBOGQhlUm6wS94EkpN8dTIC7-2GniC3pqOt6CpNU'   
+];
+
+let currentIndex = 0;
+const slide = document.getElementById('slide');
+
+function updateImage() {
+    slide.src = images[currentIndex];
+}
+
+function nextImage() {
+    currentIndex = (currentIndex + 1) % images.length;
+    updateImage();
+}
+
+function prevImage() {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    updateImage();
+}
+updateImage();
